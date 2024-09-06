d16f0b0139eee6a1fdeff0c248e968807a024db9
ambarish35 <95555806+ambarish35@users.noreply.github.com>
2024-09-06 23:46:49 +0530

    Create longestWordInASentence.java
    Find the longest word in a sentence.

---
+++ longestWordInASentence.java
@@ -0,0 +1,17 @@
+public class LongestWord {
+    public static String findLongestWord(String sentence) {
+        String[] words = sentence.split(" ");
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Java programming is interesting";
+        System.out.println("Longest word: " + findLongestWord(sentence));
+    }
+}
